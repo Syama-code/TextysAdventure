3d4db115ac39efe1a707dfc3acc5af68217ac4dd
Syama <syamadasshort1@gmail.com>
2022-08-03 11:01:51 -0500

    Updated the Adventure class

--- main_folder/main.py
+++ main_folder/main.py
@@ -2,21 +2,66 @@ import random
 
 class Player:
 
-    def __init__(self, name = input("What is your name? "), coins = 5):
-        self.name = name
-        self.coins= coins
+    def __init__(self, health = 3, coins = 5, gameover = False):
+        self.health = health
+        self.coins = coins
+        self.gameover = gameover
 
     def __repr__(self):
-        tale = f'{self.name} has {self.coins} coins.'
+        tale = f'This Player has {self.health} health points left and {self.coins} coins.'
 
         return tale 
+    
+            
 
 class Adventure:
-
-
+    
     def __init__(self):
-        pass
+        self.title = "TEXTY'S ADVENTURE"
+        self.name = input("What is your name? ")
+        self.is_good = True
 
-player1 = Player()
-print(player1)
+        def health_up(self):
+            Player().health += 1
+        
+        def health_down(self):
+            Player().health -= 1
+        
+        def coins_up(self):
+            Player().coins += 5
+
+        def coins_down(self):
+            Player().coins -= 5
+
+    def random_events(self):
+        rand = random.randint(0, )
+        events = [self.health_down, self.health_up, self.coins_down, self.coins_up]
+        good_events = 0
+        bad_events = 0
+
+        if rand % 2 == 0:
+             good_events = events[rand]
+             return good_events
+        else:
+            bad_events = events[rand]
+            self.is_good = False
+            return bad_events
+            
+
+
+    def __repr__(self):
+        left_or_right = input("Do you go left or right? ")
+        response = ''
+        if 'left' in left_or_right.lower() and self.is_good == True:
+            response = 'You decided to go left'
+        script = f"""\n*{self.title}* \n \nHello {self.name}, and welcome to Texty's Adventure.
+        All adventures start with these stats: \nHealth - {Player().health}
+                    \nCoins - {Player().coins}\n\nThroughout this adventure you will be given a choice between going
+                    right or left. There will be random encounters that happen at every turn. Good luck.
+                    \nYou begin walking down a road when you come to a fork. {left_or_right} """
+        return script
+        
+
+adventure = Adventure()
+print(adventure)
 
